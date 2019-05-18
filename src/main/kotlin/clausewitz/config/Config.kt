2d0da64da293b08b99c932ca03793285b85d2dbd
LokiSharp <loki.sharp@gmail.com>
2019-05-18 19:04:41 +0800

    style: reformat clausewitz.config

--- src/main/kotlin/clausewitz/config/Config.kt
+++ src/main/kotlin/clausewitz/config/Config.kt
@@ -2,16 +2,42 @@ package clausewitz.config
 
 val dir = System.getProperty("user.dir") + "/data/hoi4"
 val unitsFileList = listOf(
-    "anti_tank.txt", "anti_tank_brigade.txt", "anti-air.txt", "anti-air_brigade.txt", "artillery.txt",
-    "artillery_brigade.txt", "cavalry.txt", "engineer.txt", "field_hospital.txt", "heavy_armor.txt",
-    "infantry.txt", "light_armor.txt", "logistics.txt", "maintenance.txt", "medium_armor.txt",
-    "military_police.txt", "modern_armor.txt", "recon.txt", "signal.txt", "sp_anti-air_brigade.txt",
-    "sp_artillery_brigade.txt", "super_heavy_armor.txt", "tank_destroyer_brigade.txt"
+    "anti_tank.txt",
+    "anti_tank_brigade.txt",
+    "anti-air.txt",
+    "anti-air_brigade.txt",
+    "artillery.txt",
+    "artillery_brigade.txt",
+    "cavalry.txt",
+    "engineer.txt",
+    "field_hospital.txt",
+    "heavy_armor.txt",
+    "infantry.txt",
+    "light_armor.txt",
+    "logistics.txt",
+    "maintenance.txt",
+    "medium_armor.txt",
+    "military_police.txt",
+    "modern_armor.txt",
+    "recon.txt",
+    "signal.txt",
+    "sp_anti-air_brigade.txt",
+    "sp_artillery_brigade.txt",
+    "super_heavy_armor.txt",
+    "tank_destroyer_brigade.txt"
 )
-
-
 val equipmentFileList = listOf(
-    "anti_air.txt", "anti_tank.txt", "artillery.txt", "guided_missiles.txt", "infantry.txt", "mechanized.txt",
-    "motorized.txt", "support.txt", "tank_heavy.txt", "tank_light.txt", "tank_medium.txt", "tank_modern.txt",
+    "anti_air.txt",
+    "anti_tank.txt",
+    "artillery.txt",
+    "guided_missiles.txt",
+    "infantry.txt",
+    "mechanized.txt",
+    "motorized.txt",
+    "support.txt",
+    "tank_heavy.txt",
+    "tank_light.txt",
+    "tank_medium.txt",
+    "tank_modern.txt",
     "tank_super_heavy.txt"
 )
