ac04a9f95871456f31e2bb6f65cdc020ea8ae815
LokiSharp <loki.sharp@gmail.com>
2019-06-30 01:00:41 +0800

    fix: fix hardness calc

--- src/test/kotlin/TestHoi4DivisionBuilder.kt
+++ src/test/kotlin/TestHoi4DivisionBuilder.kt
@@ -22,7 +22,40 @@ class TestHoi4DivisionBuilder {
             )
         )
         val result = divisionBuilder.calculateStats(divisionTemplate)
-        Assert.assertNotNull(result)
+        with(result) {
+            Assert.assertEquals(combat_width, 40.0, 0.1)
+            Assert.assertEquals(manpower, 15700.0, 0.1)
+            Assert.assertEquals(training_time, 120.0, 0.1)
+            Assert.assertEquals(hardness, 0.0, 0.01)
+            Assert.assertNotNull(need)
+            Assert.assertNotNull(priority)
+            Assert.assertEquals(fuel_consumption, 0.0, 0.1)
+
+            Assert.assertEquals(maximum_speed, 4.0, 0.1)
+            Assert.assertEquals(max_strength, 283.2, 0.1)
+            Assert.assertEquals(max_organisation, 31.8, 0.1)
+            Assert.assertEquals(default_morale, 0.21, 0.01)
+            Assert.assertEquals(recon, 1.0, 0.1)
+            Assert.assertEquals(suppression, 11.0, 0.1)
+            Assert.assertEquals(weight, 9.0, 0.1)
+            Assert.assertEquals(supply_consumption, 2.35, 0.01)
+            Assert.assertEquals(reliability_factor, 0.0, 0.1)
+            Assert.assertEquals(casualty_trickleback, 0.0, 0.1)
+            Assert.assertEquals(experience_loss_factor, 0.0, 0.1)
+            Assert.assertEquals(equipment_capture_factor, 0.0, 0.1)
+            Assert.assertEquals(entrenchment, 5.0, 0.1)
+
+            Assert.assertEquals(soft_attack, 483.0, 0.1)
+            Assert.assertEquals(hard_attack, 81.4, 0.1)
+            Assert.assertEquals(air_attack, 35.2, 0.1)
+            Assert.assertEquals(defense, 626.4, 0.1)
+            Assert.assertEquals(breakthrough, 129.4, 0.1)
+            Assert.assertEquals(armor_value, 0.0, 0.1)
+            Assert.assertEquals(ap_attack, 70.5, 0.1)
+
+            Assert.assertEquals(template, divisionTemplate)
+            Assert.assertEquals(build_cost, 2255.0, 0.1)
+        }
     }
 
     @Test
@@ -40,7 +73,40 @@ class TestHoi4DivisionBuilder {
             )
         )
         val result = divisionBuilder.calculateStats(divisionTemplate)
-        Assert.assertNotNull(result)
+        with(result) {
+            Assert.assertEquals(combat_width, 40.0, 0.1)
+            Assert.assertEquals(manpower, 16300.0, 0.1)
+            Assert.assertEquals(training_time, 180.0, 0.1)
+            Assert.assertEquals(hardness, 0.88, 0.01)
+            Assert.assertNotNull(need)
+            Assert.assertNotNull(priority)
+            Assert.assertEquals(fuel_consumption, 59.2, 0.1)
+
+            Assert.assertEquals(maximum_speed, 10.0, 0.1)
+            Assert.assertEquals(max_strength, 259.0, 0.1)
+            Assert.assertEquals(max_organisation, 25.2, 0.1)
+            Assert.assertEquals(default_morale, 0.23, 0.01)
+            Assert.assertEquals(recon, 1.0, 0.1)
+            Assert.assertEquals(suppression, 8.0, 0.1)
+            Assert.assertEquals(weight, 23.5, 0.1)
+            Assert.assertEquals(supply_consumption, 5.32, 0.01)
+            Assert.assertEquals(reliability_factor, 0.0, 0.1)
+            Assert.assertEquals(casualty_trickleback, 0.0, 0.1)
+            Assert.assertEquals(experience_loss_factor, 0.0, 0.1)
+            Assert.assertEquals(equipment_capture_factor, 0.0, 0.1)
+            Assert.assertEquals(entrenchment, 5.0, 0.1)
+
+            Assert.assertEquals(soft_attack, 744.8, 0.1)
+            Assert.assertEquals(hard_attack, 330.8, 0.1)
+            Assert.assertEquals(air_attack, 35.2, 0.1)
+            Assert.assertEquals(defense, 814.4, 0.1)
+            Assert.assertEquals(breakthrough, 641.0, 0.1)
+            Assert.assertEquals(armor_value, 78.5, 0.1)
+            Assert.assertEquals(ap_attack, 101.4, 0.1)
+
+            Assert.assertEquals(template, divisionTemplate)
+            Assert.assertEquals(build_cost, 18753.0, 0.1)
+        }
     }
 
     @Test
@@ -58,6 +124,39 @@ class TestHoi4DivisionBuilder {
             )
         )
         val result = divisionBuilder.calculateStats(divisionTemplate)
-        Assert.assertNotNull(result)
+        with(result) {
+            Assert.assertEquals(combat_width, 40.0, 0.1)
+            Assert.assertEquals(manpower, 15200.0, 0.1)
+            Assert.assertEquals(training_time, 180.0, 0.1)
+            Assert.assertEquals(hardness, 0.05, 0.01)
+            Assert.assertNotNull(need)
+            Assert.assertNotNull(priority)
+            Assert.assertEquals(fuel_consumption, 2.2, 0.1)
+
+            Assert.assertEquals(maximum_speed, 4.0, 0.1)
+            Assert.assertEquals(max_strength, 258.8, 0.1)
+            Assert.assertEquals(max_organisation, 29.0, 0.1)
+            Assert.assertEquals(default_morale, 0.21, 0.01)
+            Assert.assertEquals(recon, 1.0, 0.1)
+            Assert.assertEquals(suppression, 10.0, 0.1)
+            Assert.assertEquals(weight, 10.0, 0.1)
+            Assert.assertEquals(supply_consumption, 2.58, 0.01)
+            Assert.assertEquals(reliability_factor, 0.0, 0.1)
+            Assert.assertEquals(casualty_trickleback, 0.0, 0.1)
+            Assert.assertEquals(experience_loss_factor, 0.0, 0.1)
+            Assert.assertEquals(equipment_capture_factor, 0.0, 0.1)
+            Assert.assertEquals(entrenchment, 5.0, 0.1)
+
+            Assert.assertEquals(soft_attack, 479.2, 0.1)
+            Assert.assertEquals(hard_attack, 138.4, 0.1)
+            Assert.assertEquals(air_attack, 35.2, 0.1)
+            Assert.assertEquals(defense, 592.6, 0.1)
+            Assert.assertEquals(breakthrough, 125.2, 0.1)
+            Assert.assertEquals(armor_value, 43.1, 0.1)
+            Assert.assertEquals(ap_attack, 84.1, 0.1)
+
+            Assert.assertEquals(template, divisionTemplate)
+            Assert.assertEquals(build_cost, 2785.0, 0.1)
+        }
     }
 }
